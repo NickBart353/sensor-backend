1598caa8da4ccb555bca429967007c8d433734cd
Nicholas Bartlett <nicholas_bartlett_30.09@outlook.de>
2025-10-10 13:00:09 +0200

    Delete sensor-backend directory

--- sensor-backend/src/main/java/org/acme/GegossenRepository.java
+++
@@ -1,8 +0,0 @@
-package org.acme;
-
-import io.quarkus.hibernate.orm.panache.PanacheRepository;
-import jakarta.enterprise.context.ApplicationScoped;
-
-@ApplicationScoped
-public class GegossenRepository implements PanacheRepository<Gegossen> {
-}
