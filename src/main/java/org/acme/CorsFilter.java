764f9abfd7857b82481940c62fbfe61b32b593e5
Nicholas Bartlett <nicholas_bartlett_30.09@outlook.de>
2025-10-11 15:30:40 +0200

    runnable stuff

--- src/main/java/org/acme/CorsFilter.java
+++ src/main/java/org/acme/CorsFilter.java
@@ -1,3 +1,4 @@
+
 package org.acme;
 
 import jakarta.ws.rs.container.ContainerRequestContext;
@@ -10,9 +11,10 @@ public class CorsFilter implements ContainerResponseFilter {
 
     @Override
     public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext) {
-        responseContext.getHeaders().add("Access-Control-Allow-Origin", "http://localhost:4200");
+        responseContext.getHeaders().add("Access-Control-Allow-Origin", "*");
         responseContext.getHeaders().add("Access-Control-Allow-Headers", "origin, content-type, accept, authorization");
         responseContext.getHeaders().add("Access-Control-Allow-Credentials", "true");
         responseContext.getHeaders().add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD");
     }
 }
+
